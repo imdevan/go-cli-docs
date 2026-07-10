429e37f901996ea935c5de29864339cbf5d3dfbf
Devan Huapaya <huapayadevan@gmail.com>
2026-07-09 23:11:06 -0700

    add render to file

--- internal/templates/templates.go
+++ internal/templates/templates.go
@@ -1,7 +1,21 @@
-//go:embed *.tmpl
 package templates
 
-import "embed"
+import (
+	"embed"
+	"os"
+	"text/template"
+)
 
 // FS holds the embedded template files.
+//go:embed *.tmpl
 var FS embed.FS
+
+// RenderToFile executes the given template with data and writes the result to path.
+func RenderToFile(tmpl *template.Template, path string, data any) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return tmpl.Execute(f, data)
+}
